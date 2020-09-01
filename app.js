49ac5201c111f82f503e6635c417e317196bc15a
jrchan84 <justinrichardchan@gmail.com>
2020-08-31 19:03:02 -0700

    fixed particles stretching issue

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-particlesJS.load('particles-js', "particles.json");
+
+// The canvas sometimes initially loads in smaller than it should be and ends up stretching the animation.
+window.addEventListener('load', function() {
+      setTimeout(function(){
+          particlesJS.load('particles-js', "particles.json");
+      }, 500);
+})
